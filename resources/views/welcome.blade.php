1ed337ef5730645b9ae9715fb6a7410f78febc7a
Enzonox <enzod.leca@gmail.com>
2019-03-19 23:22:21 -0300

    Autenticaçã com a estrutura AdminTLE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Enzonal
                 </div>
 
                 <div class="links">

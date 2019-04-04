55c08340b9e71764039e387967db9096ebf9032a
Enzonox <enzod.leca@gmail.com>
2019-04-04 12:37:25 -0300

    Registrar Históricos de Recargas no Laravel

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -11,12 +11,23 @@ class Balance extends Model
 
     public function recarga($value)
     {//Aqui colocaremos a logica de incrementacao
+        $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.', '');
         $deposito = $this->save();//Comando para salvar no banco
 
-        if($deposito){
+        //registrando o historico para o usuario
+        $historico = auth()->user()->historics()->create([
+            'type' => 'I',
+             'amount' => $value, // valor do saldo atual
+             'total_before' =>$totalBefore,// valor do saldo antes da recarga
+             'total_after' => $this->amount, //total apos recarga
+             'date' => date('ymd'),
+        ]);
+
+        if($deposito && $historico){
             return[
                 'success' => true,
+
                 'message' => 'Sucesso ao recarregar'
             ];
         }
